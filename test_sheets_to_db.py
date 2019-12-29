12fc3b06612c3ae76899b1a99ae02e4fe553805c
Lauri Palonen <38961934+lauripalonen@users.noreply.github.com>
2019-12-29 14:58:05 +0200

    Delete test_sheets_to_db.py

--- test_sheets_to_db.py
+++
@@ -1,9 +0,0 @@
-import hello
-
-def test_hello():
-    assert hello.hello_world("World!") == "Hello World!"
-
-def test_user_is_greeted_on_launch(capsys):
-    hello.launch()
-    captured = capsys.readouterr()
-    assert "Welcome" in captured.out
