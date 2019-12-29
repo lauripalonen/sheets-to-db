fc9b414438f8f2acf5d4e52fa44101b65b0f375b
lauripalonen <palonen.lauri@gmail.com>
2019-12-29 14:29:56 +0200

    add path for incorrect input

--- sheets_to_db.py
+++ sheets_to_db.py
@@ -1,23 +1,23 @@
 import click
 import xlrd
+
 from DAO import Dao
+
 import sqlite3
 from sqlite3 import Error
+
 import os
 
 @click.command()
 @click.argument('filename')
 @click.argument('db_filename')
 def main(filename, db_filename):
-    if validate_spreadsheet_type(filename) and validate_db_type(db_filename):
-        click.echo("Converting \"{}\" into a database \"{}\"".format(filename, db_filename))
-        worksheet = get_spreadsheet(filename)
-        convert_to_db(worksheet, db_filename)
-        click.echo("Database '{}' created.".format(db_filename))
+    if validate_spreadsheet(filename) and validate_db(db_filename):
+        convert_to_db(filename, db_filename)
     else:
         click.echo("Process aborted.")
 
-def validate_spreadsheet_type(filename):
+def validate_spreadsheet(filename):
     file_check = os.path.exists(filename)
 
     if not file_check:
@@ -30,7 +30,7 @@ def validate_spreadsheet_type(filename):
     
     return True
 
-def validate_db_type(db_filename):
+def validate_db(db_filename):
     if os.path.exists(db_filename):
         click.echo("File already exists in the directory. Please choose a unique name for database.")
         return False
@@ -47,7 +47,10 @@ def get_spreadsheet(filename):
 
     return worksheet
 
-def convert_to_db(worksheet, db_filename):
+def convert_to_db(filename, db_filename):
+    click.echo("Converting \"{}\" into a database \"{}\"".format(filename, db_filename))
+
+    worksheet = get_spreadsheet(filename)
 
     click.echo("Creating database file...")
     new_dao = Dao(db_filename)
@@ -71,29 +74,41 @@ def convert_to_db(worksheet, db_filename):
 
     conn.close()
 
+    click.echo("Database '{}' created.".format(db_filename))
+
 def get_headers(worksheet):
     num_cols = worksheet.ncols
     num_rows = worksheet.nrows
 
     click.echo("Your spreadsheet contains {} columns and {} rows".format(num_cols, num_rows))
-    user_input = raw_input("Use first row as headers for database columns? [y/n] ")
-    click.echo()
+
 
     headers = []
 
-    if user_input == 'y':
-        click.echo("Using first row as headers for columns.")
+    while True:
+        user_input = raw_input("Use first row as headers for database columns? [y/n] ")
+        click.echo()
 
-        for col_idx in range(0, num_cols):
-            header = worksheet.cell(0, col_idx).value
-            headers.append(header)
+
+        if user_input == 'y':
+            click.echo("Using first row as headers for columns.")
+            click.echo()
+
+            for col_idx in range(0, num_cols):
+                header = worksheet.cell(0, col_idx).value
+                headers.append(header)
+            break
     
-    elif user_input == 'n':
-        click.echo("Rename column headers for database: ")
-        for col_idx in range(0, num_cols):
-            header_old = worksheet.cell(0, col_idx).value
-            header_new = raw_input("Column {} ('{}'): ".format(col_idx + 1, header_old))
-            headers.append(header_new)
+        elif user_input == 'n':
+            click.echo("Rename column headers for database: ")
+            for col_idx in range(0, num_cols):
+                header_old = worksheet.cell(0, col_idx).value
+                header_new = raw_input("Column {} ('{}'): ".format(col_idx + 1, header_old))
+                headers.append(header_new)
+            break
+
+        else:
+            click.echo("Incorrect input. Use 'y' or 'n'")
 
     return headers
 
