bf5d965c6941dff45af1dc5264439b12bae75217
lauripalonen <palonen.lauri@gmail.com>
2019-12-29 13:56:52 +0200

    init dao

---
+++ DAO.py
@@ -0,0 +1,60 @@
+import sqlite3
+from sqlite3 import Error
+
+class Dao:
+    def __init__(self, db_filename):
+        self.db_filename = db_filename
+    
+    def create_connection(self):
+        conn = None
+        try:
+            conn = sqlite3.connect(self.db_filename)
+        except Error as e:
+            print(e)
+
+        return conn
+
+    def create_table(self, table_name):
+        stmt = "CREATE TABLE IF NOT EXISTS {} (id integer PRIMARY KEY AUTOINCREMENT);".format(table_name)        
+
+        conn = self.create_connection()
+
+        try:
+            cursor = conn.cursor()
+            cursor.execute(stmt)
+        except Error as e:
+            print(e)
+    
+        if (conn):
+          conn.close
+
+    def create_column(self, table, header):
+        stmt = "ALTER TABLE {} ADD {} VARCHAR;".format(table, header)
+
+        conn = self.create_connection()
+
+        try:
+            cursor = conn.cursor()
+            cursor.execute(stmt)
+        except Error as e:
+            print(e)
+        
+        conn.close()
+
+    def insert_values(self, table, columns, values):
+        stmt = "INSERT INTO {} ({}) VALUES {};".format(table, columns, values)
+
+        print("STATEMENT: {}".format(stmt))
+
+        conn = self.create_connection()
+
+        try:
+          cursor = conn.cursor()
+          cursor.execute(stmt)
+          conn.commit()
+        except Error as e:
+          print(e)
+
+        conn.close()
+        
+
